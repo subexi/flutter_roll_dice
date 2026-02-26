694488236097aa04e8f217569eaf31cbfc907b6d
subexi <h_dittrich@yahoo.com>
2026-02-26 13:10:04 +0100

    35 hello world mit gradient BG und weißer Schrift

---
+++ android/app/src/main/kotlin/com/example/flutter_roll_dice/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_roll_dice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.red,
+                Colors.blue,
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'Hello World!',
+              style: TextStyle(
+                fontSize: 28,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
